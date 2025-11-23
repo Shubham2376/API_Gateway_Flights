2025b60bd8295bb9544f4191f15b1e6c50e710e5
Shubham2376 <shubhamtalukdar06@gmail.com>
2025-11-23 17:20:34 +0530

    Basic Setup for Api-Gateway Done

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Inside this file basic server configuration is present 
+const express = require('express');
+const {severconfig,logger} = require('./config');
+const app = express();
+const apiroutes = require('./routes');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/api',apiroutes) // after /api whatever was written was handled by apiroutes
+app.listen(severconfig.PORT,()=>{
+    console.log(`Successfully started the server on port : ${severconfig.PORT}`);
+    //logger.info("Successfully started the server",{});
+})
